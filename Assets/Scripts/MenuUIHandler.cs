bcacf5ef4559737035de389369b1f1c71420e35c
Michael Paukov <michael.paukov@gmail.com>
2022-12-22 15:20:32 +0100

    before gamemanager

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+[DefaultExecutionOrder(1000)]
+public class MenuUIHandler : MonoBehaviour
+{
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Exit()
+    {
+
+
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
+
+}
