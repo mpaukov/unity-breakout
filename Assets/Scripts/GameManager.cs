c9b617d2d51a67a7cc909bb9b4aa6f0ac72e9497
Michael Paukov <michael.paukov@gmail.com>
2022-12-23 15:35:23 +0100

    test

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -17,5 +18,33 @@ public class GameManager : MonoBehaviour
         Instance = this;
         DontDestroyOnLoad(gameObject);
     
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public string PlayerName;
+    }
+
+    public void SavePlayerName()
+    {
+        SaveData data = new SaveData();
+        data.PlayerName = PlayerName;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadPlayerName()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            PlayerName = data.PlayerName;
+        }
     }
 }
